41813a6c8486b77b3bf9c84fe385aab2ff8b4ba9
이승무 <test850317@gmail.com>
2021-05-15 15:40:06 +0900

    Add: 장바구니 조회 기능 구현  - 접속중인 유저의 장바구니 조회

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.http.response import JsonResponse
+from django.views         import View
 
-# Create your views here.
+from orders.models        import Order, OrderItem, OrderStatus
+from util.utils           import login_required
+
+
+class CartView(View):
+
+    @login_required
+    def get(self, request):
+        order       = Order.objects.get(user=request.user, order_status=OrderStatus.objects.get(status='장바구니'))
+        order_items = OrderItem.objects.filter(order=order)
+
+        cart_info = [{
+            "id"           : i,
+            "name"         : item_info.product.name,
+            "sub_category" : item_info.product.sub_category.sub_title,
+            "product_image": item_info.product.productimage_set.first().image_url,
+            "weight"       : item_info.product_option.weight,
+            "price"        : int(item_info.product_option.price),
+            "total_price"  : int(item_info.price),
+            "quantity"     : item_info.quantity
+        } for i, item_info in enumerate(order_items)]
+
+        return JsonResponse({'cart_info': cart_info}, status=200)
