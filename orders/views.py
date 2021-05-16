aee2a40cbf45e52242c1151fb01dee970b424364
이승무 <test850317@gmail.com>
2021-05-17 01:07:24 +0900

    Add: 장바구니 상품 등록/추가 기능 구현  - 장바구니에 등록된 상품은 재고 수가 감소  - (단가 * 장바구니 제품 수량) 으로 총 가격 계산  - 이미 장바구니에 등록된 상품을 추가 등록시, 개수와 가격이 합산됨  - 쿼리 최적화 및 트랜잭션 추가 필요

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+import json
+from json.decoder         import JSONDecodeError
 
-# Create your views here.
+from django.http.response import JsonResponse
+from django.views         import View
+
+from products.models      import ProductOption
+from orders.models        import OrderItem
+from util.utils           import login_required
+
+
+class CartView(View):
+
+    @login_required
+    def post(self, request):
+        try:
+            data        = json.loads(request.body)
+            products    = data.get("cart_info")
+            order_items = request.user.order_set.get(order_status__status='장바구니').orderitem_set.all()
+
+            for product in products:
+                order_id          = product["order_id"]
+                product_id        = product["id"]
+                product_option_id = product["option_id"]
+                quantity          = product["quantity"]
+
+                items             = order_items.filter(
+                    product_id=product_id,
+                    product_option_id=product_option_id,
+                    order_id=order_id)
+                
+                if items.exists():
+                    item           = items.first()
+                    item.quantity += quantity
+                    item.price     = item.product_option.price * item.quantity
+                    item.save()
+                else:
+                    item = OrderItem.objects.create(
+                        product_id=product_id,
+                        product_option_id=product_option_id,
+                        order_id=order_id,
+                        quantity=quantity,
+                        price=ProductOption.objects.get(id=product_option_id).price * quantity)
+                
+                product_option           = ProductOption.objects.get(id=product_option_id)
+                product_option.quantity -= item.quantity
+                product_option.save()
+
+            return JsonResponse({'message': 'SUCCESS'}, status=201)
+        except JSONDecodeError:
+            return JsonResponse({'message': 'EMPTY_KEYS_ERROR'}, status=400)
