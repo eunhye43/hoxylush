e259f271a2ca264ed91063dac57d97c50e659887
이승무 <test850317@gmail.com>
2021-05-17 21:50:50 +0900

    Add: 장바구니 삭제 기능 구현  - required_login 데코레이터로 로그인 체크  - 특정 유저의 장바구니 상품 중에 삭제하려는 상품을 모두 삭제  - 삭제 완료 후, 성공 메시지 반환

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
+from json.decoder         import JSONDecodeError
 
-# Create your views here.
+from django.http.response import JsonResponse
+from django.views         import View
+
+from orders.models        import OrderItem
+from util.utils           import login_required
+
+class CartView(View):
+    ORDER_STATUS = '장바구니'
+
+    @login_required
+    def delete(self, request):
+        try:
+            data                   = json.loads(request.body)
+            product_option_id_list = [product_info['option_id'] for product_info in data]
+
+            OrderItem.objects.filter(order__user=request.user, order__order_status__status=self.ORDER_STATUS, product_option_id__in=product_option_id_list).delete()
+        except JSONDecodeError:
+            return JsonResponse({'MESSAGES': 'EMPTY_ARGS_ERROR'}, status=400)
+
+        return JsonResponse({'MESSAGES': 'SUCCESS'}, status=204)
