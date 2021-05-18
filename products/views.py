c20c8ffd361c89217cd56b7202ce16994c0c2a57
kyeongraekim <kyeongray@gmail.com>
2021-05-18 17:58:09 +0900

    Fix: 상품 필터링/리스팅 기능 리뷰 수정 반영  - 필터링/리스팅 기능 수정  - 한글 검색기능 추가

--- products/views.py
+++ products/views.py
@@ -7,35 +7,38 @@ from django.db.models       import Q
 from products.models        import Category, Product, ProductImage, SubCategory, ProductTag, Tag
 
 class ProductListView(View):
-    def post(self, request):
-        category_id       = request.POST.get('category_id')
-        sub_category_id   = request.POST.get('sub_category_id')
-        keyword           = request.POST.get('keyword')
+    def get(self, request):
+        category_id       = request.GET.get('category_id')
+        sub_category_id   = request.GET.get('sub_category_id')
+        keyword           = request.GET.get('keyword')
         pagination        = int(request.GET.get('pagination', 0))
         limit             = int(request.GET.get('limit', 4))
         offset            = pagination * 4
-
+        
         if category_id or sub_category_id:
             products = Product.objects.filter(
-                Q(category_id = category_id) | 
-                Q(sub_category_id = sub_category_id))[offset:offset+limit]              
+                Q(category_id = category_id) |
+                Q(sub_category_id = sub_category_id))[offset:offset+limit]
+
+        elif keyword:
+            products= Product.objects.filter(name__contains = keyword)
+            
         else:
             products = Product.objects.all()[offset:offset+limit]
 
         product_list = [{
-            'id'               : product.id,
-            'name'             : product.name,
-            'hashtag'          : product.hashtag,
+            'id'            : product.id,
+            'name'          : product.name,
+            'hashtag'       : product.hashtag,
             'option' : [{ 
-                'option_id'        : option.id,
-                'price'            : option.price,
-                'quantity'         : option.quantity,
-                'weight'           : option.weight,
+                'option_id'     : option.id,
+                'price'         : option.price,
+                'quantity'      : option.quantity,
+                'weight'        : option.weight,
                 } for option in product.productoption_set.all()],
-            'image_url'        : product.productimage_set.first().image_url,
+            'image_url'     : product.productimage_set.first().image_url,
             'tag'    : [{
-                'id'               : tag.id, 
-                'tag'              : tag.name,
+                'id' : tag.id, 'tag': tag.name,
                 } for tag in product.tag_set.all()]
         } for product in products]
         
