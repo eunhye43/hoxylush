8adc655e03efd8104d8b0d5af6b985e8efd14d13
박은혜 <ehye0922@gmail.com>
2021-05-18 15:10:18 +0900

    Add: 제품상세페이지 views.py 작성

--- products/views.py
+++ products/views.py
@@ -1,47 +1,33 @@
 import json
-from django.http import JsonResponse
-from django.views import View
+from django.http     import JsonResponse
+from django.views    import View
 
-from products.models import Product, ProductImage, ProductOption, ProductDescription, Ingredient
+from products.models import Product, ProductImage, ProductOption, ProductDescription, Ingredient, Tag
 class ProductDetailView(View):
-    def get(self, request, product_id):
-        product = Product.objects.get(id=product_id)
-
-        product_images = []
-        productimages = ProductImage.objects.filter(product=product)
-        for productimage in productimages:
-            product_images.append(productimage.image_url)
-
-        product_descriptions = []
-        productdescriptions = product.productdescription_set.all()
-        for productdescription in productdescriptions:
-            product_descriptions.append({
-                 'description1' : productdescription.description,
-                 'image_url1' : productdescription.image_url
-                 })
-
-        ingredient_detail = []
-        ingredients = product.ingredient_set.all()
-        for ingredient in ingredients:
-            ingredient_detail.append({
-                        'description2' : ingredient.description,
-                        'image_url2' : ingredient.image_url,
-                        'name2' : ingredient.name
-                     })
+    def get(self, request, product_option_id):
+        product_option = ProductOption.objects.get(id=product_option_id)
 
         result = {
-            'id'        : product.id,
-            'name'      : product.name,
-            'hashtag'   : product.hashtag,
-            'hit'       : product.hit,
-            'video_url' : product.video_url,
-            'price'     : product.productoption_set.first().price,
-            'weight'    : product.productoption_set.first().weight,
-            'quantity'  : product.productoption_set.first().quantity,
-            'product_images' : product_images,
-            'product_descriptions' : product_descriptions,
-            'ingredient_detail' : ingredient_detail
-        }
-        
-        
+            'product_id'           : product_option.product.id,
+            'name'                 : product_option.product.name,
+            'hashtag'              : product_option.product.hashtag,
+            'hit'                  : product_option.product.hit,
+            'video_url'            : product_option.product.video_url,
+            'product_option_id'    : product_option.id,
+            'price'                : product_option.price,
+            'quantity'             : product_option.quantity,
+            'weight'               : product_option.weight,
+            'product_images'       : [productimage.image_url
+                  for productimage in ProductImage.objects.filter(product=product_option.product)],
+            'product_descriptions' : [{
+                    'description1' : productdescription.description,
+                    'image_url1'   : productdescription.image_url
+                } for productdescription in ProductDescription.objects.filter(product=product_option.product)],
+            'ingredient_detail'    : [{
+                    'description2' : ingredient.description,
+                    'image_url2'   : ingredient.image_url,
+                    'name2'        : ingredient.name
+                } for ingredient in Ingredient.objects.filter(product=product_option.product)],
+            'tag'                  : [tag.name for tag in Tag.objects.filter(product=product_option.product)]
+                }
         return JsonResponse({'result' : result}, status=200)
