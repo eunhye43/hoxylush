e7671aa16129cd167f3926e4eec4aa9761592556
이승무 <test850317@gmail.com>
2021-05-19 15:18:13 +0900

    Add: 장바구니 상품 등록/수량 수정 기능 구현     - 장바구니 등록(PATCH)         -- 상품 리스트 화면/상품 상세 화면에서 장바구니 등록하려는 상품의 수량을 설정하고 등록 버튼을 눌렀을 경우             --- 장바구니에 해당 상품이 등록되어있지 않은 경우, 상품과 수량을 그대로 신규 등록             --- 장바구니에 해당 상품이 이미 등록되어 있을 경우, 기존 수량에 가산하여 등록     - 장바구니 상품 수량 수정(PUT)         -- 장바구니 화면에서 등록된 상품의 수량을 변경했을 경우             --- 해당 상품의 변경된 수량을 그대로 등록(기존 정보에 덮어씀)

--- orders/views.py
+++ orders/views.py
@@ -1,13 +1,61 @@
+import json
+from json.decoder         import JSONDecodeError
+
 from django.http.response import JsonResponse
 from django.views         import View
 
-from orders.models        import OrderItem
-
+from orders.models        import Order, OrderItem
+from products.models      import ProductOption
 from util.utils           import login_required
 
 class CartView(View):
     ORDER_STATUS = '장바구니'
-    
+
+    @login_required
+    def put(self, request):
+        try:
+            data                = json.loads(request.body)
+            option_id           = data.get('option_id')
+            quantity            = data.get('quantity')
+            order_item          = OrderItem.objects.get(order__user=request.user, order__order_status__status=self.ORDER_STATUS, product_option__id=option_id)
+
+            order_item.price    = order_item.product_option.price * quantity
+            order_item.quantity = quantity
+            order_item.save()
+        except JSONDecodeError:
+            return JsonResponse({'MESSAGES': 'EMPTY_ARGS_ERROR'}, status=400)
+
+        return JsonResponse({'MESSAGES': 'SUCCESS'}, status=200)
+
+    @login_required
+    def patch(self, request):
+        try:
+            data                   = json.loads(request.body)
+            option_id              = data.get('option_id')
+            quantity               = data.get('quantity')
+            product_option         = ProductOption.objects.get(id=option_id)
+
+            order_item, is_created = OrderItem.objects.get_or_create(
+                product            = product_option.product,
+                order              = Order.objects.get(user=request.user, order_status__status=self.ORDER_STATUS),
+                product_option_id  = option_id,
+                defaults           = {
+                    'price'    : 0,
+                    'quantity' : 0
+                }
+            )
+
+            if is_created:
+                order_item.quantity  = quantity
+            else:
+                order_item.quantity += quantity
+            order_item.price         = quantity * product_option.price
+            order_item.save()
+        except JSONDecodeError:
+            return JsonResponse({'MESSAGES': 'EMPTY_ARGS_ERROR'}, status=400)
+
+        return JsonResponse({'MESSAGES': 'SUCCESS'}, status=200)
+
     @login_required
     def get(self, request):
         order_items = OrderItem.objects.filter(order__user=request.user, order__order_status__status=self.ORDER_STATUS)
